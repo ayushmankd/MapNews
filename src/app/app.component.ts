5d41c4d5a4784e4a38bd2002149345bbc56d5cbe
ayushmankd <ayushmankd@live.com>
2022-05-03 11:58:30 +0530

    Initialize Map with Current Coordinates.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'map_news';
+export class AppComponent implements OnInit {
+  private map!: L.Map;
+  private centroid!: L.LatLngExpression;
+  constructor() {
+    this.centroid = [0.0, 0.0];
+  }
+  ngOnInit() {
+    this.getCurrentLocationFromUser();
+  }
+  private initializeMap(currentLocation: GeolocationPosition): void {
+    console.log(currentLocation.coords)
+    const { latitude, longitude } = currentLocation.coords;
+    this.centroid = [latitude, longitude];
+    this.map = L.map('map', {
+      center: this.centroid,
+      zoom: 12
+    })
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 10,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+    console.log("MAP: ", this.map)
+  }
+  private getCurrentLocationFromUser(): void {
+    try {
+      navigator.geolocation.getCurrentPosition((coordinates) => this.initializeMap(coordinates));
+    } catch (error) {
+      console.error("ERROR: ", error);
+      // TO Do Default Coords
+    } 
+  }
 }
